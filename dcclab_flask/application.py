5a94bd9d933792ee9ecf2c0bad48397d46e9f72e
Yeonwoo <Yeonwoo@DESKTOP-M7GQC1M>
2019-09-23 22:38:21 +0900

    test commit

--- dcclab_flask/application.py
+++ dcclab_flask/application.py
@@ -3,12 +3,12 @@ Created on 2019. 9. 23.
 
 @author: Yeonwoo
 '''
-from flask import Flask, render_template
-app=Flask(__name__)
-
-@app.route('/')
-def mainpage():
-    return render_template("index.html")
-
-if __name__ == '__main__':
-    app.run()
+# from flask import Flask, render_template
+# app=Flask(__name__)
+# 
+# @app.route('/')
+# def mainpage():
+#     return render_template("index.html")
+# 
+# if __name__ == '__main__':
+#     app.run()
